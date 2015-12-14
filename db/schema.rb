87cb81047a9268abffb7030cb849abbb2dc25bed
Prathamesh Sonpatki <csonpatki@gmail.com>
2015-12-14 17:34:54 +0530

    Add pg_stat_statements if it does not exist
    - It already exists in production database. To avoid conflicts with
      db/schema.rb when production dump is taken, this migration is added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021160718) do
+ActiveRecord::Schema.define(version: 20151214120117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "data_dumps", force: :cascade do |t|
     t.text     "data"
